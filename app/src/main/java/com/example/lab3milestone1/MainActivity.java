ba39f6c681635998381ed10f57963e74ce87f787
Sofia Khan <sakhan8@wisc.edu>
2020-02-17 14:30:46 -0600

    Lab 3 Milestone 1 Sofia Khan

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.lab3milestone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
     }
+
+        public void openDialog(){
+            ExampleDialog exampleDialog = new ExampleDialog();
+            exampleDialog.show(getSupportFragmentManager(), "example dialog");
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu){
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.example_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item){
+            switch(item.getItemId()) {
+                case R.id.item1:
+                    Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item2:
+                    Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.item3:
+                    Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.subtitle1:
+                    Toast.makeText(this, "Subtitle 1 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.subtitle2:
+                    Toast.makeText(this, "Subtitle 2 selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                default: return super.onOptionsItemSelected(item);
+            }
+        }
+
+
 }
+
